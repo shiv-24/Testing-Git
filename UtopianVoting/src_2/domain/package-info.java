a344976de4e4371f1a97b596063304871549ed53
shiv-24 <shivaggarwal.shivam1107@gmail.com>
2018-12-05 00:08:19 -0500

    remove the unwanted src folder

--- UtopianVoting/src_2/domain/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * All the domain entities are in this package.
- * 
- * @author Yogesh
- *
- */
-package domain;
--- UtopianVoting/src_2/ui/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains UI related classes and logic to bootstrap the application.
- * 
- * @author Yogesh
- *
- */
-package ui;
