4aa8ca707e304e3cf4124d5032b1f50d8bf2879f
shiv-24 <shivaggarwal.shivam1107@gmail.com>
2019-02-15 11:53:23 -0500

    tetsing Webhook

--- gfg/src/main/java/codingPractice/gfg/SubS.java
+++ gfg/src/main/java/codingPractice/gfg/SubS.java
@@ -5,7 +5,7 @@ public class SubS {
 	public static void main(String[] args) throws Exception {
 
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = "abc";
+		String s = "abcd";
 
 		subString(s, s.length());
 //		subS(s, s.length());
